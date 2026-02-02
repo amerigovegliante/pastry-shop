831f99bd59ce4d0ca75a02a310d0ca339c904a2e
Giovanni <72951567+sass0lino@users.noreply.github.com>
2026-02-02 17:31:52 +0100

    fix vari input

--- site/php/torte-pasticcini.php
+++ site/php/torte-pasticcini.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(E_ALL); //attiva visualizzazione errori
+error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require_once "dbConnection.php";
@@ -11,48 +11,42 @@ if ($paginaHTML === false) {
     exit;
 }
 $db = new DBAccess();
-$connessione = $db->openDBConnection(); //tento la connessione
+$connessione = $db->openDBConnection(); 
 
 //VARIABILI
-//script per item
-$listaItem= "";
-$tipo="";
-$tipoTitolo="";
-$titolo="";
-$LinkPagina="";
-$NessunaDisponibilità="";
-$suffisso="";
-/*
-if (isset($_GET['tipo']) && $_GET['tipo']==='torte'){
-	$NessunaDisponibilità="nessuna torta disponibile";
-	$tipo="torta";
-    $titolo="Le nostre torte";
-	$LinkPagina="<li id='currentLink'>Le nostre torte</li><li><a href=\"pasticcini\">I nostri pasticcini</a></li>";
-}else if (isset($_GET['tipo']) && $_GET['tipo']==='pasticcini'){
-	$tipo="pasticcino";
-	$NessunaDisponibilità="nessun pasticcino disponibile";
-    $titolo="I nostri pasticcini";
-    $LinkPagina="<li><a href=\"torte\">Le nostre torte</a></li><li id='currentLink'>I nostri pasticcini</li>";
-}else if (isset($_GET['tipo']) && $_GET['tipo']!=='pasticcini' && $_GET['tipo']!=='torte'){ //oppure si potrebbe andare su pag404
-    $titolo="Le nostre scelte";
-    $listaItem="Scegli tra la nostra selezione di <a href=\"../php/torte-pasticcini.php?tipo=torte\">torte</a> o <a href=\"../php/torte-pasticcini.php?tipo=pasticcini\">pasticcini</a>";
-    $LinkPagina="<li><a href=\"torte\">Le nostre torte</a></li><li><a href=\"pasticcini\">I nostri pasticcini</a></li>";
-}*/
+$listaItem = "";
+$tipo = "";
+$tipoTitolo = "";
+$titolo = "";
+$LinkPagina = "";
+$NessunaDisponibilità = "";
+$suffisso = "";
+
 if ($page === 'torte'){
-    $NessunaDisponibilità="nessuna torta disponibile";
-	$tipo="torta";
-    $tipoTitolo="torte";
-    $suffisso="\porzione";
-    $titolo="Torte";
-	$LinkPagina="<li id='currentLink'>Torte</li><li><a href=\"pasticcini\">Pasticcini</a></li>";
-}else if ($page === 'pasticcini'){
-	$tipo="pasticcino";
-    $tipoTitolo="pasticcini";
-	$NessunaDisponibilità="nessun pasticcino disponibile";
-    $titolo="Pasticcini";
-    $LinkPagina="<li><a href=\"torte\">Torte</a></li><li id='currentLink'>Pasticcini</li>";
+    $NessunaDisponibilità = "nessuna torta disponibile";
+    $tipo = "torta";
+    $tipoTitolo = "torte";
+    $suffisso = " / porzione";
+    $titolo = "Torte";
+
+    $LinkPagina = "<li id='currentLink' aria-current='page'>Torte</li><li><a href=\"pasticcini\">Pasticcini</a></li>";
+
+} else if ($page === 'pasticcini'){
+    $tipo = "pasticcino";
+    $tipoTitolo = "pasticcini";
+    $NessunaDisponibilità = "nessun pasticcino disponibile";
+    $titolo = "Pasticcini";
+
+    $LinkPagina = "<li><a href=\"torte\">Torte</a></li><li id='currentLink' aria-current='page'>Pasticcini</li>";
+
+} else {
+
+    http_response_code(404);
+    include __DIR__ . '/404.php';
+    exit;
 }
-// leggo i dati delle torte
+
+// RECUPERO DATI DAL DB
 if($connessione && empty($listaItem)){
     $Items = $db->getListOfActiveItems($tipo);
     $db->closeDBConnection();
@@ -60,21 +54,24 @@ if($connessione && empty($listaItem)){
     if (!empty($Items)){
         $listaItem .= '<ul id="grigliaTorte" class="contenuto">';
         foreach($Items as $Item){
+            // Immagini e Alt Text (con fallback)
             $imgSrc = !empty($Item['immagine']) ? "site/img/" . $Item['immagine'] : "site/img/placeholder.jpeg";
-            $altText = !empty($Item['testo_alternativo']) ? $Item['testo_alternativo'] : "Immagine non disponibile";
+            $altText = !empty($Item['testo_alternativo']) ? $Item['testo_alternativo'] : "Foto del dolce " . $Item['nome'];
+            
+            // Creazione Card con Escape Output Sicuro
             $listaItem .="<li> <article class=\"cardTorta\">     
                 <a href=\"dettagli?ID=".urlencode($Item['id'])."\"> 
-                   <img src=\"" . htmlspecialchars($imgSrc)
-                   . "\" alt=\"" . htmlspecialchars($altText) . "\">
+                   <img src=\"" . htmlspecialchars($imgSrc, ENT_QUOTES, 'UTF-8') . "\" 
+                        alt=\"" . htmlspecialchars($altText, ENT_QUOTES, 'UTF-8') . "\">
                     <div class=\"infoTorta\"> 
-                        <h3>".htmlspecialchars($Item['nome'])."</h3> 
+                        <h3>".htmlspecialchars($Item['nome'], ENT_QUOTES, 'UTF-8')."</h3> 
                         <span class=\"prezzoItem\">€".number_format($Item['prezzo'], 2, ',', '.'). $suffisso."</span> 
                     </div>
                 </a>
              </article> </li>";
         }
         $listaItem .= '</ul>';
-    }else{
+    } else {
         $listaItem ="<p class='contenuto'>Al momento non abbiamo $NessunaDisponibilità, però se volete chiamarci siamo più che felici di ascoltare la vostra richiesta</p>";
     }
 } else {
@@ -83,10 +80,11 @@ if($connessione && empty($listaItem)){
     exit;
 }
 
-//RIMPIAZZO I SEGNAPOSTO [...]
+// SOSTITUZIONE SEGNAPOSTI
 $paginaHTML = str_replace("[grigliaItems]", $listaItem, $paginaHTML);
 $paginaHTML = str_replace("[LinkPagina]", $LinkPagina, $paginaHTML);
 $paginaHTML = str_replace("[titolo]", $titolo, $paginaHTML);
 $paginaHTML = str_replace("[tipo]", $tipoTitolo, $paginaHTML);
+
 echo $paginaHTML;
 ?>
