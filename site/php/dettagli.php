ab0f4e1068ff7b6acded7cc266bc98bf6e6b51c2
Giulia2424 <giuliarom4@gmail.com>
2026-02-02 02:03:19 +0100

    da dettagli prodotto dopo aggiunta a carrello torna a torte o pasticcini

--- site/php/dettagli.php
+++ site/php/dettagli.php
@@ -84,8 +84,8 @@ if($connessione){
         // SEZIONE FORM DI ACQUISTO
         $formAcquisto .= "<section class=\"acquistoItem\">
                 <form method=\"post\" action=\"carrello\">
-                    <input type=\"hidden\" name=\"ID\" value=\"".htmlspecialchars($Item['id'])."\">";
-                    
+                    <input type=\"hidden\" name=\"ID\" value=\"".htmlspecialchars($Item['id'])."\">
+                    <input type=\"hidden\" name=\"paginaRitorno\" value=\"<?php echo ($tipoItem === 'torta') ? 'torte' : 'pasticcini'; ?>\">";
         // --- LOGICA TORTE ---
         if($tipoItem === 'torta'){
             $tipoBreadcrumb = "<a href=\"torte\">Torte</a>";
